afb1cf29a814907a3341d44b0d08f5325a2599cb
nn <205666415+guessmesome@users.noreply.github.com>
2025-11-13 19:38:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const CONFIG = {
     products: {
         1: 'https://vldmnt.ru/go/stf2s7dy29',
         2: 'https://trnsfx.ru/go/sikin2fryy',
-        3: 'https://trnsfx.ru/go/s0bhnv800i',
-        4: 'https://trnsfx.ru/go/sgkisa4pea'
+        3: 'https://trnsfx.ru/go/sgkisa4pea',
+        4: 'https://trnsfx.ru/go/s0bhnv800i'
     }
 };
